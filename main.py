7542537b9cb2cecf10943a6e36323e37e8fad9dc
rkangers12 <ranveerkang4@gmail.com>
2022-09-07 16:36:34 +0100

    rearrange and clean main script

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import discord
 import os
-
 from json import loads
 from dotenv import load_dotenv
 
@@ -9,7 +8,8 @@ from util.session_tracker import SessionTrack
 
 
 load_dotenv()
-client = discord.Client()
+intents = discord.Intents.all()
+client = discord.Client(intents=intents)
 
 datastore = Handler()
 studytrack = SessionTrack('study_records', 'studying', datastore=datastore)
@@ -28,6 +28,79 @@ async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
 
+async def snapshot(message):
+    """take a snapshot of the database"""
+
+    await message.delete()
+    datastore.snapshot()
+    await message.channel.send("**Your database has been backed up in a secure location.**")
+
+async def study(message):
+    """activate/deactivate study mode"""
+
+    await message.delete()
+
+    if not datastore.get_value('studying'):
+        studytrack.activate()
+        await message.channel.send("**Study Mode Activated...**")
+
+    else:
+        studied = studytrack.deactivate()
+        await message.channel.send("**Study Mode Deactivated...** \n  - Time Studied: {}".format(studied))
+
+async def get_study(message):
+    """retrieve study session information"""
+
+    await message.delete()
+
+    if datastore.get_value('studying'):
+        await message.channel.send("You began studying: **{}**".format(studytrack.get_session_start()))
+
+    else:
+        await message.channel.send("No currently **active** study session...")
+
+async def gym(message):
+    """activate/deactivate gym session"""
+
+    await message.delete()
+
+    if not datastore.get_value('gymming'):
+        gymtrack.activate()
+        await message.channel.send("**Gym Session Activated...**")
+
+    else:
+        exercised = gymtrack.deactivate()
+        await message.channel.send("**Gym Session Deactivated...** \n  - Exercise duration: {}".format(exercised))
+
+async def get_gym(message):
+    """get gym session information"""
+
+    await message.delete()
+
+    if datastore.get_value('gymming'):
+        await message.channel.send("You began gymming: **{}**".format(gymtrack.get_session_start()))
+
+    else:
+        await message.channel.send("No **active** gym session...")
+
+async def help(message):
+    """display helper information regarding RELAB"""
+
+    await message.delete()
+    greeting = "Hi, Welcome to 'Ranveer's Easy Life Autonomy Bot' or 'RELAB' for short."
+    message2 = "I see that you have requested some help, here is a breakdown of my current services:"
+    cmd1 = "    - .hello    -> I provide a greeting."
+    cmd2 = "    - .snapshot -> I perform a backup of your existing data to keep it extra safe. xD"
+    cmd3 = "    - .study    -> I `activate/deactivate` study mode and make a log of the session."
+    cmd4 = "    - .getstudy -> I tell you how long your study session has been active."
+    cmd5 = "    - .gym    -> I `activate/deactivate` gym mode and make a log of the session."
+    cmd6 = "    - .getgym -> I tell you how long your gym session has been active."
+    cmd7 = "    - .help     -> I provide some help to yourself, as is occurring now!"
+
+    support = '```{}\n\n{}\n\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n```'.format(greeting, message2, cmd1, cmd2, cmd3, cmd4, cmd5, cmd6, cmd7)
+    
+    await message.channel.send(support)
+
 @client.event
 async def on_message(message):
 
@@ -43,65 +116,22 @@ async def on_message(message):
             await message.channel.send("Hi {}, in the {} channel!".format(message.author, message.channel))
                 
     if msg.startswith('.snapshot'):
-        await message.delete()
-        datastore.snapshot()
-        await message.channel.send("**Your database has been backed up in a secure location.**")
+        await snapshot(message)
 
     if msg.startswith('.study'):
-        await message.delete()
-
-        if not datastore.get_value('studying'):
-            studytrack.activate()
-            await message.channel.send("**Study Mode Activated...**")
-
-        else:
-            studied = studytrack.deactivate()
-            await message.channel.send("**Study Mode Deactivated...** \n  - Time Studied: {}".format(studied))
+        await study(message)
 
     if msg.startswith('.getstudy'):
-        await message.delete()
-
-        if datastore.get_value('studying'):
-            await message.channel.send("You began studying: **{}**".format(studytrack.get_study_start()))
-
-        else:
-            await message.channel.send("No currently **active** study session...")
+        await get_study(message)
 
     if msg.startswith('.gym'):
-        await message.delete()
-
-        if not datastore.get_value('gymming'):
-            gymtrack.activate()
-            await message.channel.send("**Gym Session Activated...**")
-
-        else:
-            exercised = gymtrack.deactivate()
-            await message.channel.send("**Gym Session Deactivated...** \n  - Exercise duration: {}".format(exercised))
+        await gym(message)
 
     if msg.startswith('.getgym'):
-        await message.delete()
-
-        if datastore.get_value('gymming'):
-            await message.channel.send("You began gymming: **{}**".format(gymtrack.get_session_start()))
-
-        else:
-            await message.channel.send("No **active** gym session...")
+        await get_gym(message)
 
     if msg.startswith('.help'):
-        await message.delete()
-        greeting = "Hi, Welcome to 'Ranveer's Easy Life Autonomy Bot' or 'RELAB' for short."
-        message2 = "I see that you have requested some help, here is a breakdown of my current services:"
-        cmd1 = "    - .hello    -> I provide a greeting."
-        cmd2 = "    - .snapshot -> I perform a backup of your existing data to keep it extra safe. xD"
-        cmd3 = "    - .study    -> I `activate/deactivate` study mode and make a log of the session."
-        cmd4 = "    - .getstudy -> I tell you how long your study session has been active."
-        cmd5 = "    - .gym    -> I `activate/deactivate` gym mode and make a log of the session."
-        cmd6 = "    - .getgym -> I tell you how long your gym session has been active."
-        cmd7 = "    - .help     -> I provide some help to yourself, as is occurring now!"
-
-        support = '```{}\n\n{}\n\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n```'.format(greeting, message2, cmd1, cmd2, cmd3, cmd4, cmd5, cmd6, cmd7)
-        
-        await message.channel.send(support)
+        await help(message)
 
 
 client.run(os.getenv('TOKEN'))
